07cf5777797b6938b2cbe294c23fa029ab90fe79
Sophie Zalamar <pczalamar@gmail.com>
2025-11-12 15:46:36 -0500

    merged changes

--- test_calculator.py
+++ test_calculator.py
@@ -32,8 +32,8 @@ class TestCalculator(unittest.TestCase):
 
     ######## Partner 2
       def test_divide_by_zero(self): # 1 assertion
-        with self.assertRaises(ZeroDivisionError):
-            div(5, 0)
+          with self.assertRaises(ZeroDivisionError):
+              div(5, 0)
 
       def test_logarithm(self): # 3 assertions
 
