7e31be47d7c29aa55a82f33b4c7ec647f938f82b
Sophie Zalamar <pczalamar@gmail.com>
2025-11-12 08:54:50 -0500

    merged changes

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,19 @@
 import unittest
-from calculator import *
+import unittest
+import math
+from calculator import add, sub, div, log
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-4, 9), 5)
+        self.assertAlmostEqual(add(2.5, 0.1), 2.6, 12)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(10, 3), 7)
+        self.assertEqual(sub(-2, -8), 6)
+        self.assertAlmostEqual(sub(5.5, 2.2), 3.3, 12)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +24,33 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(5, 0)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+        self.assertAlmostEqual(log(10, 100), 2.0, 12)
+
+        self.assertAlmostEqual(log(2, 8), 3.0, 12)
+
+        self.assertAlmostEqual(log(3, 27), math.log(27, 3), 12)
+
+    def test_log_invalid_base(self): # 1 assertion
+
+        with self.assertRaises(ValueError):
+            log(0, 10)
+        with self.assertRaises(ValueError):
+            log(-2, 10)
+
+        with self.assertRaises(ValueError):
+            log(1, 10)
+
+        with self.assertRaises(ValueError):
+            log(10, 0)
+        with self.assertRaises(ValueError):
+            log(10, -5)
+
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
